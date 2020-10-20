5961421de01a7d827641782209ef7acd52e36f69
Hansel-Guzman-Soto <guzmanh@wwu.edu>
2020-10-19 21:03:37 -0700

    ghetto fix, changed the event listener to click

--- extension/ext/fb.js
+++ extension/ext/fb.js
@@ -1,63 +1,65 @@
+const selectors = {
+  feed: "[role='feed']",
+  thread: "div[data-testid='Keycommand_wrapper_feed_story']",
+  comments: ".ecm0bbzt.e5nlhep0.a8c37x1j",
+  text: ".kvgmc6g5.cxmmr5t8.oygrvhab.hcukyx3x.c1et5uql",
+  post: "div[data-ad-comet-preview='message']",
+};
+
+// let timeout = null;
+
 function scrapeData() {
   if (window.location.href == "https://www.facebook.com/") {
     console.log("success");
-
-    const selectors = {
-      feed: "[role='feed']",
-      thread: "div[data-testid='Keycommand_wrapper_feed_story']",
-      comments: ".ecm0bbzt.e5nlhep0.a8c37x1j",
-      text: ".kvgmc6g5.cxmmr5t8.oygrvhab.hcukyx3x.c1et5uql",
-      post: "div[data-ad-comet-preview='message']",
-    };
-    let timeout = null;
-    let observer = new MutationObserver(onMutation);
     let feed = document.querySelector(selectors.feed);
-
+    let observer = new MutationObserver(onMutation);
     onMutation();
-    watch();
-
-    async function onMutation() {
-      console.log("Handling mutation.");
-
-      let threads, thread, post, cmts, doc;
-
-      threads = sel(document, selectors.thread);
-
-      for (thread of threads) {
-        doc = new Thread();
-        post = posts(thread);
-
-        cmts = sel(thread, selectors.comments);
-        post.map((em) => doc.post.push(em.textContent));
-        cmts.map((em) => doc.comments.push(em.textContent));
-
-        if (doc.comments.length) {
-          await doc.save();
-        }
-      }
-    }
-
-    async function watch() {
-      observer.observe(feed, {
-        childList: true,
-        subtree: true,
-      });
-    }
-
-    function pause() {
-      observer.disconnect();
-    }
-
-    function posts(thread) {
-      return sel(thread, selectors.post);
-    }
-
-    function comments(thread) {
-      return sel(thread, selectors.text);
-    }
+    watch(feed, observer);
   } else {
     console.log("failure");
   }
 }
 
-window.addEventListener("load", scrapeData);
+async function onMutation() {
+  console.log("Handling mutation.");
+
+  let threads, thread, post, cmts, doc;
+
+  threads = sel(document, selectors.thread);
+
+  for (thread of threads) {
+    doc = new Thread();
+    post = posts(thread);
+
+    cmts = sel(thread, selectors.comments);
+    post.map((em) => doc.post.push(em.textContent));
+    cmts.map((em) => doc.comments.push(em.textContent));
+
+    if (doc.comments.length) {
+      await doc.save();
+    }
+  }
+}
+
+async function watch(feed, observer) {
+  console.log(feed);
+
+  observer.observe(feed, {
+    childList: true,
+    subtree: true,
+  });
+}
+
+function pause() {
+  observer.disconnect();
+}
+
+function posts(thread) {
+  return sel(thread, selectors.post);
+}
+
+function comments(thread) {
+  return sel(thread, selectors.text);
+}
+
+window.addEventListener("click", scrapeData);
