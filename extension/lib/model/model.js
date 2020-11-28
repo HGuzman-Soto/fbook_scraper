b21773b599d5a933c1d317b9e499460803c7de1b
hannahbuzard1 <46615766+hannahbuzard1@users.noreply.github.com>
2020-11-28 10:57:35 -0800

    Updated to extract comment id

--- extension/lib/model/model.js
+++ extension/lib/model/model.js
@@ -1,18 +1,26 @@
 class Model {
   constructor() {
     this.id = null;
+    this.commentid = null;
   }
 
   async save() {
+    var commentsLen = localStorage.getItem("commLength");
+    var numComments = commentsLen;
     let table = this.constructor.name.toLowerCase() + "s";
-
     this.id = md5(toDocument(this));
-
+    var i;
+    var commentArr = [];
+    for (i = 0; i < numComments; i++) {
+      var char ='abcdefghijklmnopqrstuvwxyz'.charAt(i);
+      commentArr.push(md5(toDocument(this) + char));
+    }
+    this.commentid = commentArr;
     let rows = await select(table);
     let found = false;
 
     for (let key in rows) {
-      if (rows[key].id === this.id) {
+      if (rows[key].id === this.id ) {
         rows[key] = this;
         found = true;
       }
@@ -25,3 +33,4 @@ class Model {
     return save(table, rows);
   }
 }
+
