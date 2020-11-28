87429c780cd228ec601536f9e39967775edf5cff
Hansel-Guzman-Soto <guzmanh@wwu.edu>
2020-11-28 01:59:38 -0800

    merging this branch really quick

--- data/preprocess.py
+++ data/preprocess.py
@@ -12,7 +12,7 @@ from nltk.corpus import stopwords
 from nltk import word_tokenize
 from nltk import StanfordTagger
 
-# relook at documentation, also this method is slow as shit right now
+# relook at documentation, also this method is slow
 
 
 def remove_entities(text):
